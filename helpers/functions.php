96359d3adb97b85950a37545351498da7364e6ec
Alexandr <258428@mail.ru>
2016-03-13 15:13:44 +0700

    Manage helpers functions

--- helpers/functions.php
+++ helpers/functions.php
@@ -1,14 +1,47 @@
 <?php
 
-if (! function_exists('e')) {
+use yii\helpers\Url;
+use yii\helpers\ArrayHelper;
+
+if (!function_exists('e')) {
     /**
      * Escape HTML entities in a string.
      *
-     * @param  string  $value
+     * @param  string $value
      * @return string
      */
     function e($value)
     {
         return htmlentities($value, ENT_QUOTES, 'UTF-8', false);
+    }
+}
+
+if (!function_exists('url')) {
+    /**
+     * Creates a URL based on the given parameters.
+     *
+     * @param string $url
+     * @param bool $scheme
+     * @return string
+     */
+    function url($url = '', $scheme = false)
+    {
+        return Url::to($url, $scheme);
+    }
+}
+
+if (!function_exists('t')) {
+    /**
+     * Translates a message to the specified language.
+     *
+     * @param $message
+     * @param array $params
+     * @param string $category
+     * @param null $language
+     * @return string
+     */
+    function t($message, $params = [], $category = 'app', $language = null)
+    {
+        return Yii::t($category, $message, $params, $language);
     }
 }
