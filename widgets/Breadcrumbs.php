7c82f7d2f3e269c894bc0520518f41e5a3a509cf
Alexandr <258428@mail.ru>
2016-03-14 00:43:37 +0700

    Add Breadcrumbs widgets

---
+++ widgets/Breadcrumbs.php
@@ -0,0 +1,35 @@
+<?php
+namespace app\widgets;
+
+use Yii;
+use yii\helpers\Url;
+use yii\widgets\Breadcrumbs as YiiBreadcrumbs;
+
+class Breadcrumbs extends YiiBreadcrumbs
+{
+    /**
+     * @inheritdoc
+     */
+    public $options = ['class' => 'crumbs'];
+    /**
+     * @inheritdoc
+     */
+    public $itemTemplate = "<li>{link}</li><li>&#187;</li>";
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        $this->homeLink = [
+            'label' => 'asd',
+            'url' => Url::home(),
+        ];
+
+        if (isset($this->getView()->params['breadcrumbs'])) {
+            $this->links = $this->getView()->params['breadcrumbs'];
+        } else {
+            $this->links = [];
+        }
+    }
+}
