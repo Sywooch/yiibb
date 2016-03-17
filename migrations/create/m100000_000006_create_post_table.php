578e0fda5b35856010f9d572a62b6c980e560cb7
Alexandr <258428@mail.ru>
2016-03-18 01:03:07 +0700

    Added post table migration

---
+++ migrations/create/m100000_000006_create_post_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use yii\db\Migration;
+
+class m100000_000006_create_post_table extends Migration
+{
+    public $tableName = '{{%post}}';
+
+    public function up()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+
+        $this->createTable($this->tableName, [
+            'id' => $this->primaryKey(),
+            'topic_id' => $this->integer()->notNull()->unsigned(),
+            'user_id' => $this->integer()->notNull()->unsigned(),
+            'user_ip' => $this->bigInteger(20),
+            'username' => $this->string(40)->notNull(),
+            'text' => $this->text(),
+            'edited_at' => $this->timestamp()->notNull(),
+            'edited_by' => $this->integer()->notNull()->unsigned(),
+            'status_id' => $this->boolean()->notNull()->defaultValue(true),
+            'updated_at' => $this->timestamp()->notNull(),
+            'created_at' => $this->timestamp()->notNull(),
+        ], $tableOptions);
+    }
+
+    public function down()
+    {
+        $this->dropTable($this->tableName);
+    }
+}
