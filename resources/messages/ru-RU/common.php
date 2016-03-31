11167f9b142f2b895540e61d36ea0f31a82c8f95
Alexandr <258428@mail.ru>
2016-03-31 08:37:18 +0700

    Remove Messages

--- resources/messages/ru-RU/common.php
+++ resources/messages/ru-RU/common.php
@@ -1,15 +1,6 @@
 <?php
 
 return [
-    // Misc
-    'Page number' => 'Страница {page}',
-
-    // Info page
-    'Info' => 'Информация',
-
-    // Rules
-    'Forum rules' => 'Правила пользования',
-
     // Menu widget
     'Index' => 'Главная',
     'User list' => 'Пользователи',
@@ -20,26 +11,4 @@ return [
     'Profile' => 'Профиль',
     'Logout' => 'Выход',
     'Administration' => 'Администрирование',
-
-    // Date
-    'Never' => 'Никогда',
-
-    // Titles
-    'Guest' => 'Гость',
-    'Member' => 'Пользователь',
-
-    // Stuff for the navigator (top of every page)
-    'Not logged in' => 'Вы вошли как гость.',
-    'Last visit' => 'Последнее посещение: %s',
-    'Topics' => 'Темы: ',
-    'New posts header' => 'Новые',
-    'Active topics' => 'активные',
-    'Unanswered topics' => 'без ответов',
-    'Posted topics' => 'Ваши',
-    'Show new posts' => 'Найти темы с новыми сообщениями с вашего последнего визита.',
-    'Show active topics' => 'Найти темы со свежими сообщениями.',
-    'Show unanswered topics' => 'Найти темы без ответов.',
-    'Show posted topics' => 'Найти темы, где вы оставляли сообщения.',
-    'Mark all as read' => 'Пометить всё как прочтённое',
-    'Mark forum read' => 'Пометить раздел как прочтённый',
 ];
