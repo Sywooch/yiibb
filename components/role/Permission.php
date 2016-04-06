43b5924debb0ff059930ef4e12d7b5eb864eb593
Alexandr <258428@mail.ru>
2016-04-07 00:27:17 +0700

    Added role system

---
+++ components/role/Permission.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace app\components\role;
+
+class Permission extends Item
+{
+    /**
+     * @inheritdoc
+     */
+    public $type = self::TYPE_PERMISSION;
+}
---
+++ components/role/rules/UpdateProfile.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Администратор
+ * Date: 04.04.2016
+ * Time: 23:36
+ */
